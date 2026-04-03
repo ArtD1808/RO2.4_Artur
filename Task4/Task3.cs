849c49985cd5cf72068b2039adea9f8ee0be4e2c
ArtD1808 <arturkurmashev2000@gmail.com>
2026-04-03 17:15:15 +0500

    Create Task3.cs

---
+++ Task4/Task3.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "apple", "banana", "cherry", "date" };
+
+        string[] manual = (string[])words.Clone();
+        for (int i = 0; i < manual.Length / 2; i++)
+        {
+            string temp = manual[i];
+            manual[i] = manual[manual.Length - 1 - i];
+            manual[manual.Length - 1 - i] = temp;
+        }
+        Console.WriteLine(string.Join(" ", manual));
+
+        string[] builtIn = (string[])words.Clone();
+        Array.Reverse(builtIn);
+        Console.WriteLine(string.Join(" ", builtIn));
+    }
+}
